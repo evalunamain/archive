ed1551bbc2227812429d6e6412f621f1c3201a5d
EvaandTim <evaandtim@appacademy.io>
2014-11-26 14:15:37 -0500

    Adds cli and visit, user, and shortened_url methods

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -9,6 +9,22 @@ class ShortenedUrl < ActiveRecord::Base
     :foreign_key => :submitter_id,
     :primary_key => :id)
 
+  has_many(
+    :visits,
+    :class_name => "Visit",
+    :foreign_key => :short_url_id,
+    :primary_key => :id
+    )
+
+
+
+  has_many(
+    :visitors,
+    -> { distinct },
+    :through => :visits,
+    :source => :visitor
+    )
+
   def self.random_code
     unique = false
 
@@ -23,6 +39,18 @@ class ShortenedUrl < ActiveRecord::Base
   def self.create_for_user_and_long_url!(user, long_url)
     short_url = random_code
     ShortenedUrl.create!( submitter_id: user.id, long_url: long_url, short_url: short_url)
+  end
+
+  def num_clicks
+    visits.count
+  end
+
+  def num_uniques
+    visitors.count
+  end
+
+  def num_recent_uniques
+    visits.select(:visitor_id).where("created_at > ?", 10.minutes.ago).distinct.count
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,18 @@ class User < ActiveRecord::Base
     :foreign_key => :submitter_id,
     :primary_key => :id
   )
+
+  has_many(
+    :visits,
+    :class_name => "Visit",
+    :foreign_key => :visitor_id,
+    :primary_key => :id
+  )
+
+  has_many(
+    :visited_urls,
+    -> { distinct },
+    :through => :visits,
+    :source => :shortened_url
+    )
 end
