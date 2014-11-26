be7aa36848ab2a813cb9581057af19779ccf7a07
EvaandTim <evaandtim@appacademy.io>
2014-11-26 17:35:18 -0500

    Adds most popular urls method.

--- app/models/tag_topic.rb
+++ app/models/tag_topic.rb
@@ -13,4 +13,35 @@ class TagTopic < ActiveRecord::Base
     :source => :url
   )
 
+  def most_popular_urls(n)
+
+    tagged_urls
+      .joins('JOIN visits ON shortened_urls.id = visits.short_url_id')
+      .group("shortened_urls.id")
+      .order('count(visits.id) desc')
+      .limit(n)
+      .pluck(:long_url)
+    #
+    # most_popular = ShortenedUrl.find_by_sql(<<-SQL)
+    # SELECT
+    #   shortened_urls.long_url
+    # FROM
+    #   shortened_urls
+    # JOIN
+    #   taggings ON (taggings.url_id = shortened_urls.id)
+    # JOIN
+    #   tag_topics ON (tag_topics.id = taggings.topic_id)
+    # JOIN
+    #   visits ON (shortened_urls.id = visits.short_url_id)
+    # WHERE
+    #   tag_topics.id = 1
+    # GROUP BY
+    #   shortened_urls.id
+    # ORDER BY
+    #   COUNT(visits.id) DESC
+    # LIMIT
+    #   5
+    # SQL
+
+  end
 end
