ed30b6d4a94c62f237f2624fdff404b674decaa4
Peter + Eva <peter+eva@appacademy.io>
2014-12-01 14:22:58 -0500

    On to the bonus round

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
 
   validates :user_name, :presence => true, :uniqueness => true
-  
+
 
   has_many(
     :authored_polls,
@@ -17,7 +17,29 @@ class User < ActiveRecord::Base
     :primary_key => :id
   )
 
-
-
+  def completed_polls
+    Poll.find_by_sql(<<-SQL)
+      SELECT
+        polls.*, COUNT(questions.id) AS count_questions
+      FROM
+        questions
+      JOIN
+        polls ON questions.poll_id = polls.id
+      JOIN
+        answer_choices ON answer_choices.question_id = questions.id
+      LEFT OUTER JOIN (
+        SELECT
+          *
+        FROM
+          responses
+        WHERE
+          responses.user_id = 1
+        ) AS r ON answer_choices.id = r.answer_choice_id
+      GROUP BY
+        polls.id
+      HAVING
+        COUNT(questions.id) = COUNT(r.id)
+    SQL
+  end
 
 end
