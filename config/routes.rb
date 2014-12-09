8f7853d97504fee717881345043f4c79f2df34ad
Eva & Jesus <eva+jesus@appacademy.io>
2014-12-09 18:06:59 -0500

    Removes unnecessary iteration on comments.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  root to: "subs#index"
+
   resource :session, only: [:new, :create, :destroy]
 
   resources :users
 
   resources :subs, only: [:index, :new, :create, :edit, :update, :show, :destroy]
 
-  resources :posts, only: [:create, :edit, :show, :update, :new, :index] 
+  resources :posts, only: [:create, :edit, :show, :update, :new, :index]
 
   resources :comments, only: [:create, :show]
 end
